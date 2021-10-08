c5112f1a6b821c3e6ea08ad1af59916ac0280ca3
JustinFK <faakjustin2@gmail.com>
2021-10-08 19:54:19 +0200

    initial setup

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+</script>
+
+<main>
+	<h1>Hello World</h1>
+</main>
+
+<style lang="scss">
+	:global(:root) {
+		--bg: #ffffff;
+		--text: #000000;
+	}
+
+	:global(*) {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		outline: none;
+	}
+
+	:global(html) {
+		scroll-behavior: smooth;
+	}
+
+	:global(body, button, input) {
+		font-family: 'Inter', sans-serif;
+		font-size: 14px;
+		background: var(--bg);
+		color: var(--text);
+	}
+</style>
